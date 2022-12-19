1bcccbab20c8d45c2fd89ec041a4d4feecd2bbec
Vishu <vishugowda2018@gmail.com>
2022-12-19 10:16:24 +0530

    Changes

--- index.js
+++ index.js
@@ -1,31 +1,51 @@
 var express = require('express');
 var ejs = require('ejs');
-var port = 8080;
+var port = 8000;
 var app = express();
-const Pool = require('pg').Pool
+// const Pool = require('pg').Pool
 var cors = require('cors')
+const sdk = require('api')('@candidapi/v1.0#gt6359l8nax80e');
 const bodyParser = require("body-parser");    
+
+
 app.use(bodyParser.json());       
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine','ejs');
+sdk.auth('237b55da00144f3fb2ce3fc3189cc537');
 
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'api',
-  password: 'newpass',
-  port: 5432,
-})
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'api',
+//   password: 'newpass',
+//   port: 5432,
+// })
 
-pool.connect((err) => {
-  if (err) throw err;
-  console.log("Connected to postgreSQL");
-});
+// pool.connect((err) => {
+//   if (err) throw err;
+//   console.log("Connected to postgreSQL");
+// });
+
 
 app.get("/", (req, res) => {
   res.sendFile("./index.html", {
       root: __dirname,
   });
+});
+
+
+app.get("/candid", (req, res) => {
+  sdk.eligible({ein: '56-2618866', provider: 'apple'})
+    .then(({ data }) => res.json(data))
+    .catch(err => console.error(err));
+});
+
+app.post("/candid", (req, res) => {
+  // console.log('ein', req.body.eIn)
+  // console.log('Provider', req.body.provider)
+  sdk.eligible({ein: `${req.body.eIn}`, provider: `${req.body.provider}`})
+    .then(({ data }) => res.json(data))
+    .catch(err => console.error(err));
 });
 
 
